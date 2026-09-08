d8ce2234d8903f3fedc8fcb80c49e7acc33105c8
Varun KV <varunkvvarunkv@gmaile.com>
2026-09-08 23:04:55 +0530

    problem on pointers

---
+++ p3.py
@@ -0,0 +1,12 @@
+class Solution:
+    def moveZeroes(self, nums: list[int]) -> None:
+
+        j = 0
+
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[j] = nums[i]
+                j += 1
+
+        for i in range(j, len(nums)):
+            nums[i] = 0
